a5fde1247b3534a003ed7910a74fcf74c0a7af1b
Freddie Juza <fjuza@client01.lab.local>
2015-10-15 20:44:27 +0200

    New functions, installing httpd and passenger
    Added basic commands for clients.
    Extended server command funtionality.
    Now installing apache, and passenger.
    Some basic commands for setting up puppet is also
    written, this is not tested at the moment thus
    code is commented.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,39 @@
 #!/usr/bin/bash
+shopt -s nocasematch
 #Download and install PuppetLabs repo for EL7.
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-
-if [ $1="server" ]; then
+if [[ $1 = "server" ]]; then
+  #open puppet port in firewalld
   firewall-cmd --zone=public --add-port=8140/tcp --permanent
   firewall-cmd --reload
-
+  
+  #install puppet, apache and other dependencies
+  yum install puppet-server -y -q -e 0
+  yum install httpd \
+	      httpd-devel \ 
+	      mod_ssl \
+	      ruby-devel \
+	      rubygems \
+	      gcc-c++ \
+	      curl-devel \
+	      zlib-devel \
+	      make \
+	      automake \
+	      openssl-devel -y -q -e 0
+  #install rack / Passenger
+  gem install rack passenger
+  #install passenger-module
+  passenger-install-apache2-module --auto
+  #start puppet master
+  ##systemctl start puppetmaster.service
+  #run puppet master on startup
+  ##puppet resource service puppetmaster ensure=running enable=true
+ 
+  #create virtual hostfile for puppet.
+  ##touch /etc/httpd/conf.d/puppetmaster.conf 
+  ##echo "puppetmaster.conf created. Configure the VirtualHost!"  
 fi
 
- 
+if [[ $1 = "client" ]]; then
+  yum install puppet -y
+fi
