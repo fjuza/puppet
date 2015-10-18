02918a2e698e4042e59a49b7eec962a1372ec3de
Freddie Juza <freddie.juza@syspartner.se>
2015-10-18 16:50:43 +0200

    Fixed formatting
    Added some reasonable output and fixed some strange formatting. Added elevation check, script now requires root-priviliges to run.

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,57 @@
 #!/usr/bin/bash
+if [ "$(id -u)" != 0 ]; then
+  echo "Script must be executed as Root!" 1>&2
+  exit 1
+fi
+#This is not a good solution.
+echo "Setting SELinux in permissive mode."
+setenforce 0
 shopt -s nocasematch
 #Download and install PuppetLabs repo for EL7.
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 if [[ $1 = "server" ]]; then
-  #open puppet port in firewalld
-  firewall-cmd --zone=public --add-port=8140/tcp --permanent
-  firewall-cmd --reload
-  
+  echo "Starting installation!"
+  #Adding extra repos.
+  curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
   #install puppet, apache and other dependencies
-  echo "installing puppet server"
+  echo "Installing puppetmaster."
   yum install puppet-server -y -q -e 0
+  echo "Done installing puppetmaster."
+  echo "Starting puppetmaster.service"
   systemctl start puppetmaster.service
   puppet resource service puppetmaster ensure=running enable=true
-  echo "Puppet is installed and running."
-  echo "Adding phusionpassenger repo"
-  curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+  echo "Installing epel-release package."
+  yum install epel-release -y -q -e 0
   echo "installing httpd, httpd-devel, mod-ssl, ruby-devel, rubygems, gcc-c++, curl-devel, zlib-devel, make, automake, openssl-devel, mod_passenger"
-  yum install epel-release -y -q -e 1
   yum install httpd httpd-devel mod_ssl ruby-devel rubygems gcc-c++ curl-devel zlib-devel make automake openssl-devel mod_passenger -y -q -e 0
- 
+  echo "Installation done. Starting configuration."
+  #open puppet port in firewalld
+  echo "Updating firewall rules"
+  firewall-cmd --zone=public --add-port=8140/tcp --permanent
+  firewall-cmd --reload
+  echo "Done"
   #create virtual hostfile for puppet.
-  #touch /etc/httpd/conf.d/puppetmaster.conf 
+  echo "Editing and moving puppetmaster.conf"
   sed -i -- "s/<CERT>/$HOSTNAME/g" puppetmaster.conf
   cp puppetmaster.conf /etc/httpd/conf.d/puppetmaster.conf
   echo "puppetmaster.conf moved to /etc/httpd/conf.d/puppetmaster.conf"  
+  echo "stopping puppetmaster service."
   service puppetmaster stop
-  service httpd start
   chkconfig puppetmaster off
+  echo "Starting httpd service"
+  service httpd start
   chkconfig  httpd on 
-  #Move rack config.
+  echo "Creating directory: /usr/share/puppet/rack/puppetmasterd"
   mkdir -p /usr/share/puppet/rack/puppetmasterd
+  echo "Creating directory: /usr/share/puppet/rack/puppetmasterd/public"
+  echo "Creating directory: /usr/share/puppet/rack/puppetmasterd/tmp"
   mkdir /usr/share/puppet/rack/puppetmasterd/public /usr/share/puppet/rack/puppetmasterd/tmp
+  echo "Moving /usr/share/puppet/ext/rack/config.ru to /usr/share/puppet/rack/puppetmasterd/"
   cp /usr/share/puppet/ext/rack/config.ru /usr/share/puppet/rack/puppetmasterd/
+  echo "Updating ownership of config.ru to puppet:puppet"
   chown puppet:puppet /usr/share/puppet/rack/puppetmasterd/config.ru
+  echo "Updating puppet.conf, adding certname, autosign and dns_alt_names options."
   sed -i -- "s/\[master\]/\[master\]\ncertname = $HOSTNAME\nautosign = true\ndns_alt_names = puppet,$HOSTNAME\n/g" /etc/puppet/puppet.conf
-  #Set SElinux in permissive mode. TODO: fix SElinux permissions.
-  setenforce 0
 fi
 
 if [[ $1 = "client" ]]; then
@@ -44,6 +60,4 @@ if [[ $1 = "client" ]]; then
   sed -i -- "s/\[agent\]/\[agent\]\nserver = puppet.lab.local\nreport = true\npluginsync = true\n/g" /etc/puppet/puppet.conf
   chkconfig puppet on
   puppet agent --daemonize
-  ##Set SELinux in permissive mode. TODO: FIX THIS!!!
-  setenforce 0
 fi
